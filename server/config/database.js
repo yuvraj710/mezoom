1e650ae154240d2b81f36547c697ea0d8f56e33b
Yuvraj Singh <ys751693@gmail.com>
2025-09-14 21:17:27 +0530

    Update database.js

--- server/config/database.js
+++ server/config/database.js
@@ -5,17 +5,10 @@ let pool;
 const connectDB = async () => {
   try {
     pool = new Pool({
-      host: process.env.DB_HOST,
-      port: process.env.DB_PORT || 5432,
-      database: process.env.DB_NAME,
-      user: process.env.DB_USER,
-      password: process.env.DB_PASSWORD,
+      connectionString: process.env.DATABASE_URL,
       ssl: {
         rejectUnauthorized: false,
       },
-      max: 20,
-      idleTimeoutMillis: 30000,
-      connectionTimeoutMillis: 2000,
     });
 
     const client = await pool.connect();
